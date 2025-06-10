a3a97a7c9751eb7aa41ec8761283fdd51529787b
Achmad Rizal Muttaqin <achmadrizalmuttaqin@comestoarra.com>
2025-06-10 22:12:01 +0700

    minimal probing depth Tooth_14

--- js/1/14.js
+++ js/1/14.js
@@ -786,29 +786,29 @@ function toggle_PI_14_mp() {
     }
 }
 
-function change_probing_14() {
+function change_probing_14(probing_depth_min = 0) {
     if (Tooth_14 == 1) {
         
     // Variablen aus dem Formular
-        st_14_db = document.forms[0]["st_14_db"].value;
-        st_14_b = document.forms[0]["st_14_b"].value;
-        st_14_mb = document.forms[0]["st_14_mb"].value;
+        st_14_db = document.forms[0]["st_14_db"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_db"].value;
+        st_14_b = document.forms[0]["st_14_b"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_b"].value;
+        st_14_mb = document.forms[0]["st_14_mb"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_mb"].value;
 
-        if (document.forms[0]["st_14_dp"].value > 3) {document.forms[0]["st_14_dp"].style.color = 'red';} else {document.forms[0]["st_14_dp"].style.color = 'black'}
-        if (document.forms[0]["st_14_p"].value > 3) {document.forms[0]["st_14_p"].style.color = 'red';} else {document.forms[0]["st_14_p"].style.color = 'black'}
-        if (document.forms[0]["st_14_mp"].value > 3) {document.forms[0]["st_14_mp"].style.color = 'red';} else {document.forms[0]["st_14_mp"].style.color = 'black'}
+        if (document.forms[0]["st_14_dp"].value > probing_depth_min) {document.forms[0]["st_14_dp"].style.color = 'red';} else {document.forms[0]["st_14_dp"].style.color = 'black'}
+        if (document.forms[0]["st_14_p"].value > probing_depth_min) {document.forms[0]["st_14_p"].style.color = 'red';} else {document.forms[0]["st_14_p"].style.color = 'black'}
+        if (document.forms[0]["st_14_mp"].value > probing_depth_min) {document.forms[0]["st_14_mp"].style.color = 'red';} else {document.forms[0]["st_14_mp"].style.color = 'black'}
         
         mg_14_db = document.forms[0]["mg_14_db"].value;
         mg_14_b = document.forms[0]["mg_14_b"].value;
         mg_14_mb = document.forms[0]["mg_14_mb"].value;
         
-        st_14_dp = document.forms[0]["st_14_dp"].value;
-        st_14_p = document.forms[0]["st_14_p"].value;
-        st_14_mp = document.forms[0]["st_14_mp"].value;
+        st_14_dp = document.forms[0]["st_14_dp"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_dp"].value;
+        st_14_p = document.forms[0]["st_14_p"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_p"].value;
+        st_14_mp = document.forms[0]["st_14_mp"].value - probing_depth_min <= 0 ? 0 : .forms[0]["st_14_mp"].value;
 
-        if (document.forms[0]["st_14_db"].value > 3) {document.forms[0]["st_14_db"].style.color = 'red';} else {document.forms[0]["st_14_db"].style.color = 'black'}
-        if (document.forms[0]["st_14_b"].value > 3) {document.forms[0]["st_14_b"].style.color = 'red';} else {document.forms[0]["st_14_b"].style.color = 'black'}
-        if (document.forms[0]["st_14_mb"].value > 3) {document.forms[0]["st_14_mb"].style.color = 'red';} else {document.forms[0]["st_14_mb"].style.color = 'black'}
+        if (document.forms[0]["st_14_db"].value > probing_depth_min) {document.forms[0]["st_14_db"].style.color = 'red';} else {document.forms[0]["st_14_db"].style.color = 'black'}
+        if (document.forms[0]["st_14_b"].value > probing_depth_min) {document.forms[0]["st_14_b"].style.color = 'red';} else {document.forms[0]["st_14_b"].style.color = 'black'}
+        if (document.forms[0]["st_14_mb"].value > probing_depth_min) {document.forms[0]["st_14_mb"].style.color = 'red';} else {document.forms[0]["st_14_mb"].style.color = 'black'}
         
         mg_14_dp = document.forms[0]["mg_14_dp"].value;
         mg_14_p = document.forms[0]["mg_14_p"].value;
